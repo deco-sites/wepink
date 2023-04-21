7574f258fb4992cfeed84944f0bb2dc53682d5bb
Vavarine <evailson.m.barbosa@gmail.com>
2023-04-21 10:35:11 -0300

    initial config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 // Daisi UI theme
 export const theme = {
   "primary": "hsl(180 100% 10%)",
-  "secondary": "hsl(0 39% 39%)",
+  "secondary": "hsl(330, 100%, 50%)",
   "accent": "hsl(150 100% 50%)",
-  "neutral": "hsl(0 0% 20%)",
+  "neutral": "hsl(0 0% 0%)",
   "base-100": "hsl(0 0% 100%)",
   "success": "hsl(150 62% 95%)",
   "warning": "hsl(43 100% 95%)",
